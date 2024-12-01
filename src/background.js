10e85426c619c2282c07639a3049832d9e31396b
Roman Hentschke <roman.hentschke@amdis-services.com>
2024-12-01 16:52:00 +0100

    Added support for new Salesforce setup domains (e.g., .sandbox.my.salesforce.com, .my.salesforce-setup.com). Extracted salesforce-utils.js to encapsulate common utility functions for better modularity and reusability.

--- src/background.js
+++ src/background.js
@@ -1,12 +1,11 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+import {
+    salesforceUrlPatterns,
+    getAPIHostAndHeaders,
+} from './salesforce-utils.js';
 
-const salesforceUrlPattern = '.lightning.force.com';
-
+const salesforceVersion = 'v62.0';
 
 async function toJson(response) {
     return await response.json()
@@ -23,7 +22,7 @@ async function queryLanguagesFromSalesforce(apiHost, headers) {
 }
 
 async function setUserLanguage(userId, userLanguage, apiHost, headers) {
-    return await fetch(apiHost + '/services/data/v52.0/sobjects/User/' + userId, {
+    return await fetch(apiHost + '/services/data/' + salesforceVersion + '/sobjects/User/' + userId, {
         method: 'PATCH',
         headers: headers,
         body: JSON.stringify({
@@ -35,13 +34,13 @@ async function setUserLanguage(userId, userLanguage, apiHost, headers) {
 }
 
 async function getUserInfo(apiHost, headers) {
-    return await fetch(apiHost + '/services/data/v50.0/chatter/users/me', {headers: headers}).then(toJson).then(async data => {
+    return await fetch(apiHost + '/services/data/' + salesforceVersion + '/chatter/users/me', {headers: headers}).then(toJson).then(async data => {
         return data.id;
     });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (!request.payload || !request.payload.message || !request.payload.message.currentUrl || !request.payload.message.currentUrl.includes(salesforceUrlPattern)) {
+    if (!request.payload || !request.payload.message || !request.payload.message.currentUrl || !salesforceUrlPatterns.some(pattern => request.payload.message.currentUrl.includes(pattern))) {
         sendResponse(null);
         return;
     }
@@ -63,28 +62,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     })();
     return true;// keep the messaging channel open for sendResponse
 });
-
-async function getAPIHostAndHeaders(currentUrl) {
-    const url = new URL(currentUrl);
-    let protocol = url.protocol;
-    let host = url.host;
-    let customDomain = host.substring(0, host.indexOf(".lightning.force.com"));
-    return new Promise((resolve, reject) => {
-        chrome.cookies.getAll({"domain": "salesforce.com", "name": "sid"}, function (cookies) {
-            resolve(cookies);
-        });
-    }).then(cookies => {
-        let apiHost;
-        let headers;
-        for (let cookie of cookies) {
-            if (cookie.domain.startsWith(customDomain + ".")) {
-                let token = cookie.value;
-                let domain = cookie.domain;
-                apiHost = protocol + '//' + domain;
-                headers = {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"};
-                break;
-            }
-        }
-        return [apiHost, headers];
-    })
-}
